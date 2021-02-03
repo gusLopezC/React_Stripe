87c54bac4c5f191aa0e4fae36b96d888e5eae802
gusLopezC <guslopezcallejas@gmail.com>
2021-02-03 15:09:28 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import "bootswatch/dist/lux/bootstrap.min.css";
+import "./App.css";
+
+const stripePromise = loadStripe(
+  "pk_test_51IGoRvBzMJ7KcpX6knJmPiuZdDanULBsmS9ILs3F68pWeaweb9uPxCBn7jvM9Llu54BYKikZWoEPwkRSetiK1wex001CbwZekN"
+);
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const element = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: element.getElement(CardElement),
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="card card-body">
+      <img src="https://riodoce.mx/wp-content/uploads/2019/11/donitas1-768x768.jpg" alt="bimbo" className="img-fluid"></img>
+      <CardElement />
+      <button>Buy</button>
+    </form>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Elements stripe={stripePromise}>
+      <div className="container p-4">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+          <CheckoutForm />
+          </div>
+        </div>
+      </div>
+    </Elements>
   );
 }
 
